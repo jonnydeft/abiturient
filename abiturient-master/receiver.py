17fbfeae7229236302574e7f5aee5a72c6c80817
HOSAGACHU <loveless1448@gmail.com>
2016-06-16 07:10:18 +0300

    ver 0.8

--- abiturient-master/receiver.py
+++ abiturient-master/receiver.py
@@ -1,34 +1,75 @@
 # -*- coding: utf-8 -*-
 import sqlite3
 import sys
-import re
 import codecs
-import io
-from pdfgen import *
+import re
+from odf.opendocument import load, OpenDocumentText
+from odf.text import P
+from odf import text, teletype
 from reportlab import *
-
-conn       = sqlite3.connect('db.sqlite3')
-#exampleODT = open('example.odt', 'rb')
-#for line in exampleODT:
-    #print(line.decode('utf8'))
-#exampleODT.read()
-
-#with open('example.odt', mode='a', encoding='utf8') as f:
- #   while index != (count_index - offset_index):
-  #      f.write(str(search_result[index + 1]))
-   #     f.write('\n')
-    #    index += 1
-
-word = u'привет'
-with io.open('example.odt', encoding='utf-8') as file:
-    for line in file:
-        if word in line:
-            print(line, end='')
-
-c          = conn.cursor()
+from ezodf import *
+def getAndinsert(cur, counter):
+    changeList = ['FamName',
+                  'Name',
+                  'Patron',
+                  'BirDay',
+                  'BirPlace',
+                  'CitShip',
+                  'Natio',
+                  'HomePhone',
+                  'MobPhone',
+                  'Registr',
+                  'Living',
+                  'PassSer',
+                  'PassNum',
+                  'Whom',
+                  'When',
+                  'MothFname',
+                  'MothName',
+                  'MothPatr',
+                  'MothPlace',
+                  'MothPos',
+                  'MothNum',
+                  'FathFname',
+                  'FathName',
+                  'FathPatr',
+                  'FathPlace',
+                  'FathPos',
+                  'FathNum',
+                  'SpecDescr',
+                  'EduName',
+                  'EduEnd',
+                  'EdSer',
+                  'EdNum',
+                  'FstNum',
+                  'SecNum',
+                  'ThrNum',
+                  'ThrSub',
+                  'GPA'
+                 ]
+    doc   = ezodf.opendoc('example.odt')
+    for i in changeList:
+        replaced = False
+        for line in doc:
+            print(line)
+            if i in line:
+                p = P(text = changeList)
+                n = P(text = dataList[counter])
+                line.replace(i, n)
+                counter += 1
+    doc.save('completed.odt')
+    
+'''Участок работы с базой данных'''
+counter    = 0
 enteredID  = input('Введите номер записи\n')
-cur        = c.execute('select surname, name, patronymic, birth, bpl, citizenship, nationality, homephone, mobilephone, registration, address, series, number, issued, issuance_date, m_surname, m_name, m_patronymic, m_job, m_post, m_phone, f_surname, f_name, f_patronymic, f_job, f_post, f_phone, speciality, finished_school, date_finished, doc_series, doc_number, math_mark, russian_mark, prof_subj_mark, avg_mark from abiturient_personalinfo where id=%s' % enteredID)
+conn       = sqlite3.connect('db.sqlite3')
+c          = conn.cursor()
+cur        = c.execute('select surname, name, patronymic, birth, bpl, citizenship, nationality, homephone, mobilephone, registration, address, series, number, issued, issuance_date, m_surname, m_name, m_patronymic, m_job, m_post, m_phone, f_surname, f_name, f_patronymic, f_job, f_post, f_phone, speciality, finished_school, date_finished, doc_series, doc_number, math_mark, russian_mark, prof_subj_mark, prof_subj, avg_mark from abiturient_personalinfo where id=%s' % enteredID)
 
+
+'''Создание списка из записи таблицы'''
+dataList   = []
 for row in cur:
-    getAndinsert(exampleODT, cur)
- 
+    dataList = str(row).split(',')
+    
+getAndinsert(dataList, counter)
